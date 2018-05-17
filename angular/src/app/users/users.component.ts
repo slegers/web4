9ee527a032289d73c6fd208e9d828ffb2532cb10
Yanice Slegers <yaniceslegers@gmail.com>
2018-05-17 23:33:28 +0200

    implemented constants

--- angular/src/app/users/users.component.ts
+++ angular/src/app/users/users.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
 
-
+const user: User[] = [
+  {firstname: 'Bram',
+    lastname: 'Slegers',
+    email: 'bram@ucll.be'},
+  {
+    firstname: 'Maarten',
+    lastname: 'Slegers',
+    email: 'maarten@ucll.be'
+  }
+];
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent implements OnInit {
-  user: User = {
-    firstname: 'Bram',
-    lastname: 'Slegers',
-    email: 'bram@ucll.be'
-  }
+  selectedUser: User;
+
+  users: User[] = user;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onSelect(u: User): void {
+    this.selectedUser = u;
+  }
 }
