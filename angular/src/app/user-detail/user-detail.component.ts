aeff05e1a5ac4f7bdc4364defdb1f8df0d28f76d
Yanice Slegers <yaniceslegers@gmail.com>
2018-05-19 23:46:16 +0200

    web4 basic finished

--- angular/src/app/user-detail/user-detail.component.ts
+++ angular/src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,7 +10,14 @@ import { User } from '../user';
 })
 export class UserDetailComponent implements OnInit {
   @Input() user: User;
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) {}
+
   ngOnInit() {
+  }
+  save(): void {
+    this.userService.updateUser(this.user);
   }
 }
